9aea7a1d66237382a1dd8815d6c705aba317a03c
achoPijo <miguelbasket92@gmail.com>
2018-06-29 13:05:44 +0200

    add ejecta analysis to max temperature analysis

--- src/utils/analysis_maximum_temperature.f90
+++ src/utils/analysis_maximum_temperature.f90
@@ -38,14 +38,14 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
  use part,         only: igas,iamtype,iphase,maxphase,rhoh
  use prompting,    only: prompt
  use setbinary,    only: L1_point
- use units,        only: umass,udist,utime,unit_density,unit_pressure
+ use units,        only: umass,udist,utime,unit_density,unit_velocity,unit_pressure
  character(len=*), intent(in)    :: dumpfile
  integer,          intent(in)    :: num,npart,iunit
  real,             intent(inout) :: xyzh(:,:),vxyzu(:,:) !due to reset center of mass
  real,             intent(in)    :: particlemass,time
 
- integer                      :: i,iloc
- real                         :: rTmax,Tmax,r
+ integer                      :: i,iloc,j,nej
+ real                         :: rTmax,Tmax,r, ri,rj,mej,rhoejm,Tejm,vejinfm,phi,bi
  logical                      :: iexist
  character(len=120)           :: fileprefix,fileout
 
@@ -90,7 +90,7 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
  else
     open(iunit,file=fileout,status='new')
     write(iunit,"('#',3(1x,'[',i2.2,1x,a11,']',2x))") &
-        1,'r',    &
+        1,'time',    &
         2,'Tmax', &
         3,'rTmax'
     
@@ -98,6 +98,72 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
 
      close(iunit)
  endif
+
+ !
+ !-- Mass Ejecta Analysis
+
+ !--------------
+
+ nej = 0
+ mej = 0.
+ rhoejm  = 0.
+ Tejm    = 0.
+ vejinfm = 0.
+ do i=1,npart
+    ri = sqrt(xyzh(1,i)**2+xyzh(2,i)**2+xyzh(3,i)**2)
+    phi= 0.
+    do j=1,npart
+       if (j /= i) then 
+          rj = sqrt(xyzh(1,j)**2+xyzh(2,j)**2+xyzh(3,j)**2)
+          phi= phi - particlemass/(abs(ri-rj)) !remember units chosen so that G=1
+       endif
+    enddo
+    bi = phi + 0.5*(sqrt(vxyzu(1,i)**2 + vxyzu(1,i)**2 + vxyzu(1,i)**2)**2) !specific orbital energy
+    if (bi > 0.) then
+       nej = nej + 1
+       mej = mej + particlemass
+       Tejm      = Tejm + vxyzu(5,i)
+       rhoejm    = rhoejm + rhoh(xyzh(4,i),particlemass)
+       vejinfm   = vejinfm + sqrt(2*bi)
+    endif
+ enddo
+
+ mej     = mej         * umass
+ Tejm    = Tejm/nej
+ rhoejm  = rhoejm/nej  * unit_density
+ vejinfm = vejinfm/nej * unit_velocity
+
+ !
+ !-- Dump information to file
+ !
+ 
+ fileout = trim(fileprefix)//'ejecta.dat'
+ inquire(file=trim(fileout),exist=iexist)
+ if (iexist) then
+    open(iunit,file=fileout,status='old',position='append')
+    
+    write(iunit,'(5(1pe18.10,1x))') time,mej,Tejm,rhoejm,vejinfm
+
+    close(iunit)
+ else
+    open(iunit,file=fileout,status='new')
+    write(iunit,"('#',5(1x,'[',i2.2,1x,a11,']',2x))") &
+        1,'time',    &
+        2,'mej', &
+        3,'Tejm', &
+        4,'rhoejm', &
+        5,'vejinfm'
+    
+    write(iunit,'(5(1pe18.10,1x))') time,mej,Tejm,rhoejm,vejinfm
+
+
+    close(iunit)
+ endif
+
+
+
+
+
 
 end subroutine do_analysis
 
