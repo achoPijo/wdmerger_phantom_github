260fc24257b93f2bb39780601673350aafa51ed8
achoPijo <miguelbasket92@gmail.com>
2018-07-18 16:46:37 +0200

    force set to artificial conductivity prescription based on thermal conductivity of ideal gas

--- src/main/force.F90
+++ src/main/force.F90
@@ -1693,7 +1693,7 @@ ifgas: if (iamgasi .and. iamgasj) then
            endif
            if (maxvxyzu == 4) dendissterm = vsigu*denij*(auterm*grkerni + autermj*grkernj)
            if (maxvxyzu == 5) then
-              dendissterm = ((alphai + alphaj)/2)*vsigu*(dtempij/(10**9))*(auterm*grkerni + autermj*grkernj)
+              dendissterm = rij1**3*vsigu*dtempij*((cvi+cvj)/2)*(auterm*grkerni + autermj*grkernj)
            endif
         endif
 
