cc072f1db57ba50c42d1648cb94b55ec10e24122
achoPijo <miguelbasket92@gmail.com>
2019-02-05 14:23:43 +0100

    problem analysis_density_profiles_Density

--- src/utils/analysis_final_profiles_density.f90
+++ src/utils/analysis_final_profiles_density.f90
@@ -101,7 +101,7 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
  rtab(1)=dr
  do i=2,nrpoints
     rtab(i)      = rtab(i-r)+dr
-    keplertab(i) = sqrt(mtot/rtab(i))
+    keplertab(i) = sqrt(mtot/rtab(i))/r
  enddo
 
  do i=1,nrpoints
