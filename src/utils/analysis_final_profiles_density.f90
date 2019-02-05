3e8772a3d2b7ab97fef4af25cda7413559ab4795
achoPijo <miguelbasket92@gmail.com>
2019-02-05 15:49:59 +0100

    problem analysis_density_profiles_Density

--- src/utils/analysis_final_profiles_density.f90
+++ src/utils/analysis_final_profiles_density.f90
@@ -46,7 +46,7 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
  real,             intent(inout) :: xyzh(:,:),vxyzu(:,:) !due to reset center of mass
  real,             intent(in)    :: particlemass,time
 
- integer, parameter  :: nrpoints = 20000
+ integer, parameter  :: nrpoints = 2000
  integer             :: i,j,ierr,ncountx,ncountz
  real                :: rmax,rTmax,Tmax,dr,mtot,r
  real                :: rtab(nrpoints),Ttabx(nrpoints),Ttabz(nrpoints),Ttab(nrpoints),rhotab(nrpoints),rhotabx(nrpoints),rhotabz(nrpoints)
@@ -67,7 +67,7 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
  ncountxtab(:)    =0.
  ncountztab(:)    =0.
  rsample = 0.001
- nrpoints = 2000
+ !nrpoints = 2000
  call prompt('rsample in code units?', rsample)
  !call prompt('number of points', nrpoints)
  mtot         = npart*particlemass
